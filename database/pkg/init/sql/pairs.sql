2bf2c7a285189c2c55fd4299041cd69319f2dbf8
RestinGreen <majairober@gmail.com>
2023-05-02 18:57:32 +0200

    updated tokens and pairs tables

--- database/pkg/init/sql/pairs.sql
+++ database/pkg/init/sql/pairs.sql
@@ -1,13 +1,10 @@
 CREATE TABLE pairs (
     id SERIAL PRIMARY KEY,
     pair_address VARCHAR(255) NOT NULL UNIQUE,
-    token0_address VARCHAR(255) NOT NULL,
-    token1_address VARCHAR(255) NOT NULL,
     reserve0 NUMERIC NOT NULL,
     reserve1 NUMERIC NOT NULL,
     last_updated INTEGER,
     dex_id INTEGER,
-    used INTEGER DEFAULT 0,
     token0_id INTEGER,
     token1_id INTEGER,
     FOREIGN KEY (token0_id) REFERENCES tokens(id),
--- database/pkg/init/sql/tokens.sql
+++ database/pkg/init/sql/tokens.sql
@@ -1,5 +1,7 @@
-CREATE TABLE tokens ( 
-    id SERIAL PRIMARY KEY, 
-    address VARCHAR(255) NOT NULL UNIQUE, symbol VARCHAR(255), 
-    name VARCHAR(255)
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(255) NOT NULL UNIQUE,
+    symbol VARCHAR(255),
+    name VARCHAR(255),
+    decimal INTEGER
 );
